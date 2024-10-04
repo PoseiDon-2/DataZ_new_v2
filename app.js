9b0d4c7c49c8da3674d92df522c46b2ee7e11b1c
PoseiDon <suriya.b@kkumail.com>
2024-10-05 00:46:56 +0700

    เชื่อม mongoDB

--- app.js
+++ app.js
@@ -5,6 +5,28 @@ const indexRouter = require('./routes/index'); // Import your routes
 app.set('view engine', 'ejs');
 
 
+
+const mongoose = require('mongoose');
+
+// ใช้ Connection string จาก MongoDB Atlas
+const uri = "mongodb+srv://suriyabarisi:<Suriya_110920>@cluster0.yhgme.mongodb.net/";
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB Atlas');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+});
+
+
+
+
+
+
 app.use('/', indexRouter); // Use the routes
 
 const port = process.env.PORT || 3000; // Define your port
